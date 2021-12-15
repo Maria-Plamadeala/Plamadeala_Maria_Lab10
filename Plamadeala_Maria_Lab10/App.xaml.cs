7a0a1f090770fcda465d9c61b2782c1f0ed3474f
maria_plamadeala@yahoo.com <maria_plamadeala@yahoo.com>
2021-12-15 20:03:30 +0200

    Add project files.

---
+++ Plamadeala_Maria_Lab10/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plamadeala_Maria_Lab10.Data;
+using System.IO;
+
+namespace Plamadeala_Maria_Lab10
+{
+    public partial class App : Application
+    {
+
+        static ShoppingListDatabase database;
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+            
+        }
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
+    }
+}
